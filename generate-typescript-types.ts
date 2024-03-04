a449861cdfbd49c6e164ff86e098afb490b3751b
SignpostMarv <me@signpostmarv.name>
2024-03-04 11:58:18 +0000

    removing generated types from repo
    removing generated types from repo until the [qa site post](https://questions.satisfactorygame.com/post/65e5367dcd33105bd53f931f) is resolved

--- generate-typescript-types.ts
+++ generate-typescript-types.ts
@@ -1,155 +1,26 @@
-import schema from './schema/update8.schema.json' assert {type: 'json'};
-import {
-	TypesGeneration,
-	TypesGenerationFromSchema,
-	TypesGenerationMatchesReferenceName,
-	GenerationMatch, ImportTracker,
-} from './lib/TypesGeneration';
-import ts from 'typescript';
-import {glob,} from 'glob';
-import {mkdir, unlink, writeFile} from "node:fs/promises";
 import {dirname} from "path";
 import {fileURLToPath} from "url";
-import {
-	target_files as color_target_files,
-	generators as color_generators,
-} from './lib/TypesGeneration/color';
-import {
-	target_files as enum_target_files,
-	generators as enum_generators,
-} from './lib/TypesGeneration/enum';
-import {
-	target_files as validator_target_files,
-	generators as validator_generators,
-} from './lib/TypesGeneration/validators';
-import {
-	target_files as vector_target_files,
-	generators as vector_generators,
-} from './lib/TypesGeneration/vectors';
-import * as prettier from 'prettier';
+
+import {DocsTsGenerator, GenerationException} from "./lib/DocsTsGenerator";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const target_files:{[key: string]: string} = Object.assign(
-	{},
-	enum_target_files,
-	color_target_files,
-	validator_target_files,
-	vector_target_files,
-);
-
-const generators:TypesGeneration<any, any>[] = [
-	...color_generators,
-	...enum_generators,
-	...validator_generators,
-	...vector_generators,
-];
-
-const supported_conversions:GenerationMatch<object>[] = Object.entries(schema.definitions).filter((e) => {
-	for (const generator of generators) {
-		if (generator.test(generator instanceof TypesGenerationFromSchema ? e[1] : e[0])) {
-			return true;
-		}
-	}
-
-	return false;
-}).map(e => {
-	return new GenerationMatch<object>(e[0], e[1], generators.find((maybe) => {
-		return maybe.test(maybe instanceof TypesGenerationFromSchema ? e[1] : e[0]);
-	}) as TypesGenerationFromSchema<object>|TypesGenerationMatchesReferenceName<object, any>);
+const generator = new DocsTsGenerator({
+	docs_path: `${__dirname}/data/Docs.json`,
+	utf8_cache_path: `${__dirname}/data/`,
 });
 
-const supported_conversion_names = supported_conversions.map(e => e.definition);
+try {
+	const progress = await generator.generate_types(`${__dirname}/generated-types/`);
 
-const unsupported_conversions = Object.keys(schema.definitions).filter(
-	(maybe) => !supported_conversion_names.includes(maybe)
-);
-
-console.log('Conversions');
-console.table({
-	'Supported': supported_conversions.length,
-	'Unsupported': unsupported_conversions.length,
-});
-
-const missing_target_files = supported_conversions.map((match) => {
-	return match.definition;
-}).filter((maybe) => !(maybe in target_files));
-
-if (missing_target_files.length) {
-	console.log(
-		'Missing target files:',
-		missing_target_files
-	);
-}
-
-if (unsupported_conversions.length) {
-	console.log(
-		'Unsupported Conversions:',
-		unsupported_conversions
-	);
-}
-
-const printer = ts.createPrinter({
-	newLine: ts.NewLineKind.LineFeed,
-});
-
-const files:{[key: string]: ts.Node[]} = {};
-
-for (const match of supported_conversions) {
-	if (!(match.definition in target_files)) {
-		throw new Error(`no target file found for ${match.definition}`);
+	console.log(progress);
+} catch (err) {
+	if (err instanceof GenerationException) {
+		console.error(err.progress);
+		console.error(err.exception);
+	} else {
+		console.error(err);
 	}
 
-	const target_file = target_files[match.definition];
-
-	if (!(target_file in files)) {
-		files[target_file] = [];
-	}
-
-	files[target_file].push(match.generation.generate(match.data, match.definition));
-}
-
-const cleanup = await glob(`${__dirname}/generated-types/update8/**/*.ts`);
-
-for (const remove of cleanup) {
-	await unlink(remove);
-}
-
-for (const entry of Object.entries(files)) {
-	const result_file = ts.createSourceFile(
-		entry[0],
-		'',
-		ts.ScriptTarget.Latest,
-		false,
-		ts.ScriptKind.TS
-	);
-
-	const nodes = [
-		...ImportTracker.generate_imports(entry[0]),
-		...entry[1]
-	];
-
-	const file_name = `${__dirname}/generated-types/update8/${entry[0]}`;
-	const dir = dirname(file_name);
-
-	await mkdir(dir, {
-		recursive: true,
-	});
-
-	await writeFile(
-		file_name,
-		await prettier.format(
-			nodes.map((node) => {
-				return printer.printNode(ts.EmitHint.Unspecified, node, result_file)
-			}).join('\n\n'),
-			{
-				parser: 'typescript',
-				singleQuote: true,
-				trailingComma: 'es5',
-				arrowParens: 'always',
-				endOfLine: 'lf',
-				useTabs: true,
-			}
-		)
-	);
+	throw new Error('Failed to generate types');
 }
---
+++ lib/DocsTsGenerator.ts
@@ -0,0 +1,288 @@
+import {mkdir, readFile, unlink, writeFile} from "node:fs/promises";
+import {basename, dirname} from 'node:path';
+import {existsSync} from 'node:fs';
+
+import * as prettier from 'prettier';
+import Ajv, {ErrorObject, JSONSchemaType} from 'ajv/dist/2020';
+
+import update8_schema from '../schema/update8.schema.json' assert {type: 'json'};
+
+import {
+	target_files as color_target_files,
+	generators as color_generators,
+} from './TypesGeneration/color';
+import {
+	target_files as enum_target_files,
+	generators as enum_generators,
+} from './TypesGeneration/enum';
+import {
+	target_files as validator_target_files,
+	generators as validator_generators,
+} from './TypesGeneration/validators';
+import {
+	target_files as vector_target_files,
+	generators as vector_generators,
+} from './TypesGeneration/vectors';
+import {
+	TypesGeneration,
+	TypesGenerationFromSchema,
+	TypesGenerationMatchesReferenceName,
+	GenerationMatch,
+	ImportTracker,
+} from './TypesGeneration';
+import {default_config} from './DocsValidation';
+import ts from "typescript";
+import {glob} from "glob";
+
+class ValidationError extends Error
+{
+	readonly errors:ErrorObject[];
+	readonly json:any;
+
+	constructor(message:string, errors:ErrorObject[]|undefined|null, json:any) {
+		super(message);
+		this.errors = errors || [];
+		this.json = json;
+	}
+}
+
+export class GenerationException
+{
+	readonly progress:generation_result;
+	readonly exception:unknown;
+
+	constructor(progress:generation_result, exception:unknown) {
+		this.progress = progress;
+		this.exception = exception;
+	}
+}
+
+declare type generation_result = {
+	definitions: {
+		keys: string[],
+		supported: string[],
+		unsupported: string[],
+		missing_target_files: string[],
+	},
+};
+
+export class DocsTsGenerator
+{
+	private readonly utf8_cache_path:string|undefined;
+	private readonly docs_path:string|object[];
+	private docs:object[]|undefined;
+
+	constructor({
+		docs_path,
+		utf8_cache_path = undefined
+	}:{
+		docs_path: string|object[],
+		utf8_cache_path?: string
+	}) {
+		this.utf8_cache_path = utf8_cache_path;
+		this.docs_path = docs_path;
+	}
+
+	private async load_from_file(filepath:string): Promise<any>
+	{
+		const file = await readFile(filepath, {
+			encoding: 'utf-16le',
+		});
+
+		const utf8 = Buffer.from(file).toString('utf-8');
+
+		return JSON.parse(utf8.trim());
+	}
+
+	private async load(): Promise<any>
+	{
+		if (undefined === this.docs) {
+			if ('string' !== typeof(this.docs_path)) {
+				return this.docs_path;
+			} else if (!this.docs_path.endsWith('.json')) {
+				throw new Error('Probably not a JSON file');
+			} else {
+				if (this.utf8_cache_path) {
+					const utf8_filename = basename(this.docs_path).replace(
+						/\.json$/,
+						'.utf8.json'
+					);
+					const utf8_filepath = `${this.utf8_cache_path}/${utf8_filename}`;
+
+					if (existsSync(utf8_filepath)) {
+						this.docs = JSON.parse((await readFile(utf8_filepath)).toString());
+					}
+
+					const json = await this.load_from_file(this.docs_path);
+
+					await writeFile(utf8_filepath, JSON.stringify(json, null, '\t') + '\n');
+
+					return json;
+				} else {
+					return await this.load_from_file(this.docs_path);
+				}
+			}
+		}
+
+		return this.docs;
+	}
+
+	private async validate<T>(json:any, schema:JSONSchemaType<T>): Promise<T>
+	{
+		default_config.ajv = new Ajv({
+			verbose: true,
+		});
+
+		const validateDocs = default_config.ajv.compile(schema);
+
+		if (!validateDocs(json)) {
+			throw new ValidationError(
+				'Argument 1 failed to validate against the Update 8 JSON Schema',
+				validateDocs.errors,
+				json
+			);
+		}
+
+		return json;
+	}
+
+	async get<T extends [
+		{
+			NativeClass: string,
+			Classes: {ClassName: string}[]
+		},
+		...{
+			NativeClass: string,
+			Classes: {ClassName: string}[]
+		}[]
+	]>(): Promise<T> {
+		return this.validate<T>(await this.load(), (update8_schema as unknown) as JSONSchemaType<T>);
+	}
+
+	/**
+	 * @throws {GenerationException}
+	 */
+	async generate_types(parent_folder:string) : Promise<generation_result>
+	{
+		const target_files:{[key: string]: string} = Object.assign(
+			{},
+			enum_target_files,
+			color_target_files,
+			validator_target_files,
+			vector_target_files,
+		);
+
+		const generators:TypesGeneration<any, any>[] = [
+			...color_generators,
+			...enum_generators,
+			...validator_generators,
+			...vector_generators,
+		];
+
+		const supported_conversions:GenerationMatch<object>[] = Object.entries(update8_schema.definitions).filter(
+			(e:[string, object]) => {
+				for (const generator of generators) {
+					if (generator.test(generator instanceof TypesGenerationFromSchema ? e[1] : e[0])) {
+						return true;
+					}
+				}
+
+				return false;
+			}
+		).map((e: [string, object]) => {
+			return new GenerationMatch<object>(e[0], e[1], generators.find((maybe) => {
+				return maybe.test(maybe instanceof TypesGenerationFromSchema ? e[1] : e[0]);
+			}) as TypesGenerationFromSchema<object>|TypesGenerationMatchesReferenceName<object, any>);
+		});
+
+		const supported_conversion_names = supported_conversions.map(e => e.definition);
+
+		const unsupported_conversions = Object.keys(update8_schema.definitions).filter(
+			(maybe) => !supported_conversion_names.includes(maybe)
+		);
+
+		const missing_target_files = supported_conversions.map((match) => {
+			return match.definition;
+		}).filter((maybe) => !(maybe in target_files));
+
+		const progress = {
+			definitions: {
+				keys: Object.keys(update8_schema),
+				supported: supported_conversion_names,
+				unsupported: unsupported_conversions,
+				missing_target_files: missing_target_files,
+			},
+		};
+
+		try {
+			const printer = ts.createPrinter({
+				newLine: ts.NewLineKind.LineFeed,
+			});
+
+			const files:{[key: string]: ts.Node[]} = {};
+
+			for (const match of supported_conversions) {
+				if (!(match.definition in target_files)) {
+					throw new Error(`no target file found for ${match.definition}`);
+				}
+
+				const target_file = target_files[match.definition];
+
+				if (!(target_file in files)) {
+					files[target_file] = [];
+				}
+
+				files[target_file].push(match.generation.generate(match.data, match.definition));
+			}
+
+			const cleanup = await glob(`${parent_folder}/update8/**/*.ts`);
+
+			for (const remove of cleanup) {
+				await unlink(remove);
+			}
+
+			for (const entry of Object.entries(files)) {
+				const result_file = ts.createSourceFile(
+					entry[0],
+					'',
+					ts.ScriptTarget.Latest,
+					false,
+					ts.ScriptKind.TS
+				);
+
+				const nodes = [
+					...ImportTracker.generate_imports(entry[0]),
+					...entry[1]
+				];
+
+				const file_name = `${parent_folder}/update8/${entry[0]}`;
+				const dir = dirname(file_name);
+
+				await mkdir(dir, {
+					recursive: true,
+				});
+
+				await writeFile(
+					file_name,
+					await prettier.format(
+						nodes.map((node) => {
+							return printer.printNode(ts.EmitHint.Unspecified, node, result_file)
+						}).join('\n\n'),
+						{
+							parser: 'typescript',
+							singleQuote: true,
+							trailingComma: 'es5',
+							arrowParens: 'always',
+							endOfLine: 'lf',
+							useTabs: true,
+						}
+					)
+				);
+			}
+		} catch (err) {
+			throw new GenerationException(progress, err);
+		}
+
+		return progress;
+	}
+}
