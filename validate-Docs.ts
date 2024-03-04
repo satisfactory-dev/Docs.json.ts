80ac89b40a476dd3766278eb42e17d1616e1433a
SignpostMarv <me@signpostmarv.name>
2024-03-04 12:27:53 +0000

    use docs generator for validation

--- validate-Docs.ts
+++ validate-Docs.ts
@@ -1,14 +1,11 @@
-import Ajv from 'ajv/dist/2020';
-import schema from './schema/update8.schema.json' assert {type: 'json'};
-import data from './data/Docs.utf8.json' assert {type: 'json'};
-import {default_config} from './lib/DocsValidation';
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {DocsTsGenerator} from './lib/DocsTsGenerator';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-default_config.ajv = new Ajv({
-	verbose: true,
+const generator = new DocsTsGenerator({
+	docs_path: `${__dirname}/data/Docs.json`,
+	utf8_cache_path: `${__dirname}/data/`,
 });
 
-const validateDocs = default_config.ajv.compile(schema);
-
-if (!validateDocs(data)) {
-	console.error(validateDocs.errors);
-}
+await generator.get();
