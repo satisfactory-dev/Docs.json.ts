3d6d8ec2de30411f71f3b8984d28346da9aa9331
SignpostMarv <me@signpostmarv.name>
2024-03-09 23:11:32 +0000

    extract functions to methods

--- lib/DocsTsAutoImports.ts
+++ lib/DocsTsAutoImports.ts
@@ -4,18 +4,17 @@ import {basename, dirname, relative} from "node:path";
 
 export class DocsTsAutoImports
 {
-	private readonly files:{[key: string]: ts.Node[]};
+	private readonly files_entries:[string, ts.Node[]][];
+	private readonly comes_from: {[key: string]: string} = {};
 	constructor(files:{[key: string]: ts.Node[]}) {
-		this.files = files;
+		this.files_entries = Object.entries(files);
 	}
 
-	async generate()
+	private file_exports(): {[key: string]: [string, ...string[]]}
 	{
 		const file_exports: {[key: string]: [string, ...string[]]} = {};
 
-		const files_entries = Object.entries(this.files);
-
-		for (const entry of files_entries) {
+		for (const entry of this.files_entries) {
 			const [filename, nodes] = entry;
 
 			const check_these = nodes.filter(
@@ -57,23 +56,10 @@ export class DocsTsAutoImports
 			}
 		}
 
-		const comes_from: {[key: string]: string} = {};
+		return file_exports;
+	}
 
-		for (const entry of Object.entries(file_exports)) {
-			const [filename, exports_these] = entry;
-
-			for (const export_name of exports_these) {
-				if (export_name in comes_from) {
-					throw new Error(`${export_name} conflict!`);
-				}
-
-				comes_from[export_name] = filename.replace(/\.ts$/, '');
-			}
-		}
-
-		const auto_imports: import_these_later = {};
-
-		function references_to_names(
+	private references_to_names(
 			filename:string,
 			nodes:ts.TypeReferenceNode[]
 		) : string[] {
@@ -103,18 +89,18 @@ export class DocsTsAutoImports
 					(
 						maybe
 					): maybe is Exclude<
-						keyof typeof comes_from,
+						keyof typeof this.comes_from,
 						number
 					> => {
 						return (
-							maybe in comes_from &&
-							filename !== `${comes_from[maybe]}.ts`
+							maybe in this.comes_from &&
+							filename !== `${this.comes_from[maybe]}.ts`
 						);
 					}
 				);
 		}
 
-		function filter_types_to_reference_nodes(nodes:ts.TypeNode[]) : ts.TypeReferenceNode[]
+	private filter_types_to_reference_nodes(nodes:ts.TypeNode[]) : ts.TypeReferenceNode[]
 		{
 			return nodes.filter(
 				(maybe) : maybe is ts.TypeReferenceNode => {
@@ -123,18 +109,21 @@ export class DocsTsAutoImports
 			);
 		}
 
-		function filter_is_tuple_type(maybe:ts.TypeNode) : maybe is ts.TupleTypeNode {
+	private filter_is_tuple_type(maybe:ts.TypeNode) : maybe is ts.TupleTypeNode {
 			return ts.SyntaxKind.TupleType === maybe.kind;
 		}
 
-		function extract_type_references_from_tuple_type_node(node:ts.TupleTypeNode) : ts.TypeReferenceNode[] {
-			return filter_types_to_reference_nodes(node.elements.filter((maybe) => {
+	private extract_type_references_from_tuple_type_node(node:ts.TupleTypeNode) : ts.TypeReferenceNode[] {
+		return this.filter_types_to_reference_nodes(
+			node.elements.filter(
+				(maybe) => {
 				return ts.SyntaxKind.NamedTupleMember !== maybe.kind;
 			}));
 		}
 
-		function reduce_type_references_arrays_to_type_reference_array(arrays:ts.TypeReferenceNode[][]) : ts.TypeReferenceNode[]
-		{
+	private reduce_type_references_arrays_to_type_reference_array(
+		arrays:ts.TypeReferenceNode[][]
+	) : ts.TypeReferenceNode[] {
 			return arrays.reduce((was, is) => {
 				was.push(...is);
 
@@ -142,19 +131,39 @@ export class DocsTsAutoImports
 			}, [] as ts.TypeReferenceNode[]);
 		}
 
-		function extract_type_references_from_type_literal_node(node:ts.TypeLiteralNode) : ts.TypeReferenceNode[] {
-			return reduce_type_references_arrays_to_type_reference_array(node.members.filter(
+	private extract_type_references_from_type_literal_node(
+		node:ts.TypeLiteralNode
+	) : ts.TypeReferenceNode[] {
+		return this.reduce_type_references_arrays_to_type_reference_array(node.members.filter(
 				(maybe) : maybe is ts.PropertySignature => {
 					return ts.SyntaxKind.PropertySignature === maybe.kind;
 				}
 			).filter((maybe) : maybe is ts.PropertySignature & {type:ts.TupleTypeNode} => {
-				return undefined !== maybe.type && filter_is_tuple_type(maybe.type);
+			return undefined !== maybe.type && this.filter_is_tuple_type(maybe.type);
 			}).map((property_signature) => {
-				return extract_type_references_from_tuple_type_node(property_signature.type);
+			return this.extract_type_references_from_tuple_type_node(property_signature.type);
 			}));
 		}
 
-		for (const entry of files_entries) {
+	async generate()
+	{
+		const file_exports = this.file_exports();
+
+		for (const entry of Object.entries(file_exports)) {
+			const [filename, exports_these] = entry;
+
+			for (const export_name of exports_these) {
+				if (export_name in this.comes_from) {
+					throw new Error(`${export_name} conflict!`);
+				}
+
+				this.comes_from[export_name] = filename.replace(/\.ts$/, '');
+			}
+		}
+
+		const auto_imports: import_these_later = {};
+
+		for (const entry of this.files_entries) {
 			const [filename, nodes] = entry;
 
 			const type_aliases = nodes.filter(
@@ -175,18 +184,19 @@ export class DocsTsAutoImports
 				[] as ts.TypeNode[]
 			);
 
-			const union_type_references = filter_types_to_reference_nodes(union_types);
+			const union_type_references = this.filter_types_to_reference_nodes(union_types);
 
-			const union_type_tuple_references = reduce_type_references_arrays_to_type_reference_array
+			const union_type_tuple_references = this.reduce_type_references_arrays_to_type_reference_array
 			(union_types.filter(
-				filter_is_tuple_type
-			).map(extract_type_references_from_tuple_type_node));
+				this.filter_is_tuple_type
+			).map((e) => this.extract_type_references_from_tuple_type_node(e)));
 
-			const union_type_literal_sub_references = reduce_type_references_arrays_to_type_reference_array(union_types.filter(
+			const union_type_literal_sub_references = this.reduce_type_references_arrays_to_type_reference_array(
+				union_types.filter(
 				(maybe) : maybe is ts.TypeLiteralNode => {
 					return ts.SyntaxKind.TypeLiteral === maybe.kind;
 				}
-			).map(extract_type_references_from_type_literal_node));
+			).map((e) => this.extract_type_references_from_type_literal_node(e)));
 
 			const class_declarations = nodes.filter(
 				(maybe): maybe is ts.ClassDeclaration => {
@@ -229,19 +239,19 @@ export class DocsTsAutoImports
 					});
 
 			const reference_names = [
-				...references_to_names(
+				...this.references_to_names(
 					filename,
 					property_declarations_use_type_reference
 				),
-				...references_to_names(
+				...this.references_to_names(
 					filename,
 					union_type_references
 				),
-				...references_to_names(
+				...this.references_to_names(
 					filename,
 					union_type_tuple_references
 				),
-				...references_to_names(
+				...this.references_to_names(
 					filename,
 					union_type_literal_sub_references
 				),
@@ -259,7 +269,7 @@ export class DocsTsAutoImports
 				}
 
 				for (const import_this of reference_names) {
-					let import_from = `${relative(dirname(filename), dirname(comes_from[import_this]))}/${basename(comes_from[import_this])}`;
+					let import_from = `${relative(dirname(filename), dirname(this.comes_from[import_this]))}/${basename(this.comes_from[import_this])}`;
 
 					if (import_from.startsWith('/')) {
 						import_from = `.${import_from}`;
