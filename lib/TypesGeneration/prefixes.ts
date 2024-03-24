1d8b25514fb6edf87d490ec83c5ff575b2d17f36
SignpostMarv <me@signpostmarv.name>
2024-03-24 11:06:12 +0000

    cleaning up unreferenced files

--- lib/TypesGeneration/prefixes.ts
+++
@@ -1,9 +0,0 @@
-export const schema = {
-	type: 'object',
-	required: ['type', 'string_starts_with'],
-	additionalProperties: false,
-	properties: {
-		type: {type: 'string', const: 'string'},
-		string_starts_with: {type: 'string', minLength: 1},
-	},
-};
