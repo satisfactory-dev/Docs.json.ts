20bd9a77aaf8aaa944b7aaec5c2ecb4f8680b147
SignpostMarv <me@signpostmarv.name>
2024-03-27 22:37:54 +0000

    fix type reference usage

--- lib/TypeDefinitionDiscovery/JsonSchema/ObjectType.ts
+++ lib/TypeDefinitionDiscovery/JsonSchema/ObjectType.ts
@@ -103,7 +103,7 @@ export class ObjectType extends GeneratorDoesDiscovery<
 			} else if ('$ref' in raw_data && is_string(raw_data.$ref)) {
 				type = ts.factory.createIntersectionTypeNode([
 					type,
-					type_reference_node(adjust_class_name(raw_data.$ref)),
+					type_reference_node(adjust_class_name(raw_data.$ref.substring(14))),
 				]);
 			}
 
