388ab6c2fa65d3ba098cc87f2f075cc734d7f0eb
SignpostMarv <me@signpostmarv.name>
2024-03-28 16:24:04 +0000

    adding coverage to type_reference_node

--- tests/lib/TsFactoryWrapper.spec.ts
+++ tests/lib/TsFactoryWrapper.spec.ts
@@ -1,10 +1,28 @@
 import 'jasmine';
 import {
-	create_modifiers, modifier_map,
+	adjust_class_name, create_literal,
+	create_modifiers,
+	modifier_map, type_reference_node,
 } from '../../lib/TsFactoryWrapper';
 import {
 	non_empty_keys,
 } from '../../lib/ArrayUtilities';
+import exp from 'node:constants';
+import ts, {LiteralTypeNode, StringLiteral} from 'typescript';
+
+describe('adjust_class_name', () => {
+	it('behaves', () => {
+		for (const entry of Object.entries({
+			'boolean': 'Docs_boolean',
+			'class': 'Docs_class',
+			'foo-bar': 'foo_bar',
+		})) {
+			const [input, expected] = entry;
+
+			expect(adjust_class_name(input)).toEqual(expected);
+		}
+	});
+});
 
 describe('create_modifiers', () => {
 	it('removes duplicates', () => {
@@ -22,3 +40,54 @@ describe('create_modifiers', () => {
 		).toHaveSize(keys.length);
 	});
 })
+
+describe('type_reference_node', () => {
+	it('behaves', () => {
+		const type = type_reference_node(
+			'foo',
+			create_literal('bar'),
+			create_literal('baz')
+		);
+
+		expect(ts.isIdentifier(type.typeName)).toBeTrue();
+		expect(type.typeArguments).toHaveSize(2);
+		expect(ts.isLiteralTypeNode((type.typeArguments || [])[0])).toBeTrue();
+		expect(ts.isLiteralTypeNode((type.typeArguments || [])[1])).toBeTrue();
+		expect(
+			ts.isStringLiteral(
+				(
+					type.typeArguments as unknown as [
+						LiteralTypeNode,
+						LiteralTypeNode
+					]
+				)[0].literal
+			)
+		).toBeTrue()
+		expect(
+			ts.isStringLiteral(
+				(
+					type.typeArguments as unknown as [
+						LiteralTypeNode,
+						LiteralTypeNode
+					]
+				)[1].literal
+			)
+		).toBeTrue()
+		expect(
+			((
+				type.typeArguments as unknown as [
+					LiteralTypeNode,
+					LiteralTypeNode
+				]
+			)[0].literal as StringLiteral).text
+		).toEqual('bar');
+		expect(
+			((
+				type.typeArguments as unknown as [
+					LiteralTypeNode,
+					LiteralTypeNode
+				]
+			)[1].literal as StringLiteral).text
+		).toEqual('baz');
+	});
+});
