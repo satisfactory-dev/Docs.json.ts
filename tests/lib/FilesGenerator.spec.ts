96fdf2d2c8f7be1e962107df00f5ddcf853f961d
SignpostMarv <me@signpostmarv.name>
2024-03-28 18:45:59 +0000

    adding test for merge_files

--- tests/lib/FilesGenerator.spec.ts
+++ tests/lib/FilesGenerator.spec.ts
@@ -1,5 +1,6 @@
 import 'jasmine';
 import {
+	FilesGenerator,
 	FromArray,
 } from '../../lib/FilesGenerator';
 import {
@@ -18,4 +19,21 @@ describe('FromArray', () => {
 
 		expect(entries).toHaveSize(1);
 	});
+});
+
+describe('merge_files', () => {
+	it('defines an object', async () => {
+		expect(await FilesGenerator.merge_files([])).toEqual({});
+	})
+	it('modifies an object', async () => {
+		const files = {};
+
+		expect(
+			await FilesGenerator.merge_files([
+				new FromArray([
+					{file: 'foo.ts', node: create_literal('bar')},
+				]),
+			], files)
+		).toEqual({'foo.ts': []});
+	})
 });
