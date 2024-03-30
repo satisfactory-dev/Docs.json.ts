caa4a1bc33f4b80e28ca4c55181c5f4e57d139e3
SignpostMarv <me@signpostmarv.name>
2024-03-30 14:23:37 +0000

    forgot to declare __dirname

--- tests/fixtures/Docs.ts
+++ tests/fixtures/Docs.ts
@@ -1,6 +1,11 @@
 import {
 	DocsTsGenerator,
 } from '../../lib/DocsTsGenerator';
+import {
+	__dirname_from_meta,
+} from '../../lib/__dirname';
+
+const __dirname = __dirname_from_meta(import.meta);
 
 export const docs = new DocsTsGenerator({
 	docs_path: `${__dirname}/../../data/Docs.json`,
