b598511fb157eb79eccdc271849414fa053d5f4b
SignpostMarv <me@signpostmarv.name>
2024-03-30 13:00:58 +0000

    test coverage for NumberStrings

---
+++ tests/lib/DataDiscovery/CustomTypes/NumberStrings.spec.ts
@@ -0,0 +1,115 @@
+import {
+	describe,
+	it,
+} from 'node:test';
+import assert from 'node:assert/strict';
+import {
+	NumberStrings, schema_type,
+} from '../../../../lib/DataDiscovery/CustomTypes/NumberStrings';
+import {
+	DataDiscovery,
+} from '../../../../lib/DataDiscovery';
+import {
+	DocsTsGenerator,
+} from '../../../../lib/DocsTsGenerator';
+import Ajv from 'ajv/dist/2020';
+import {
+	configure_ajv,
+} from '../../../../lib/DocsValidation';
+import {
+	isAsExpression,
+	isIdentifier,
+	isStringLiteral,
+	isTypeReferenceNode,
+} from '../../../../assert/TypeScriptAssert';
+import {is_instanceof} from '../../../../assert/CustomAssert';
+import {RawGenerationResult} from '../../../../lib/DataDiscovery/Generator';
+
+const __dirname = import.meta.dirname;
+
+void describe('NumberStrings.convert_unknown', async () => {
+	const docs = new DocsTsGenerator({
+		docs_path: `${__dirname}/../../../../data/Docs.json`,
+		cache_path: `${__dirname}/../../../../data/`,
+	});
+	const ajv = new Ajv({verbose: true});
+	configure_ajv(ajv);
+	const discovery = new DataDiscovery(docs, ajv);
+	const {definitions: {NativeClass}} = await docs.schema();
+	const number_strings = new NumberStrings(discovery, ajv);
+
+	void it('throws', async () => {
+		await assert.rejects(
+			number_strings.convert_unknown(
+				{
+					$ref: '#/definitions/decimal-string',
+				},
+				1
+			),
+			{
+				property: 1,
+				message: 'must be a string!',
+			},
+		);
+		await assert.rejects(
+			number_strings.convert_unknown(
+				{
+					$ref: '#/definitions/NativeClass',
+				} as unknown as schema_type,
+				'foo'
+			),
+			{
+				property: NativeClass,
+				message: 'Not a pattern schema!',
+			},
+		);
+	});
+	void it('succeeds', async () => {
+		const value = await number_strings.convert_unknown(
+			{
+				$ref: '#/definitions/decimal-string',
+			},
+			'1.2'
+		);
+
+		isAsExpression(value.expression);
+		isStringLiteral(value.expression.expression);
+		isTypeReferenceNode(value.expression.type);
+		isIdentifier(value.expression.type.typeName);
+		assert.equal(
+			value.expression.type.typeName.escapedText,
+			'StringPassedRegExp'
+		);
+	});
+});
+
+void describe('NumberStrings.matches', async () => {
+	const docs = new DocsTsGenerator({
+		docs_path: `${__dirname}/../../../../data/Docs.json`,
+		cache_path: `${__dirname}/../../../../data/`,
+	});
+	const ajv = new Ajv({verbose: true});
+	configure_ajv(ajv);
+	const discovery = new DataDiscovery(docs, ajv);
+	const {definitions: {
+		NativeClass,
+	}} = await docs.schema();
+	const number_strings = new NumberStrings(discovery, ajv);
+
+	void it('resolves undefined', async () => {
+		assert.equal(await number_strings.matches(NativeClass), undefined);
+	});
+	void it('resolves to NumberStrings', async () => {
+		const value = await number_strings.matches({
+			$ref: '#/definitions/decimal-string',
+		});
+
+		is_instanceof(value, RawGenerationResult);
+
+		const result = await value.result();
+
+		is_instanceof(result, NumberStrings);
+
+		assert.equal(result, number_strings);
+	});
+});
