3b6d61721ae9c7cff76b8f8382d1c4b35ecd3348
SignpostMarv <me@signpostmarv.name>
2024-03-30 13:12:40 +0000

    increasing coverage of Generator.find() to 100%

--- tests/lib/DataDiscovery/Generator.spec.ts
+++ tests/lib/DataDiscovery/Generator.spec.ts
@@ -4,14 +4,71 @@ import {
 } from 'node:test';
 import assert from 'node:assert/strict';
 import {
+    GenerationResult,
 	Generator,
+    RawGenerationResult,
 } from '../../../lib/DataDiscovery/Generator';
+import {
+	DocsTsGenerator,
+} from '../../../lib/DocsTsGenerator';
+import {
+	configure_ajv,
+} from '../../../lib/DocsValidation';
+import {
+	DataDiscovery,
+} from '../../../lib/DataDiscovery';
+import Ajv from 'ajv/dist/2020';
+
+const __dirname = import.meta.dirname;
 
 void describe('Generator.find', () => {
+	const docs = new DocsTsGenerator({
+		docs_path: `${__dirname}/../../../data/Docs.json`,
+		cache_path: `${__dirname}/../../../data/`,
+	});
+	const ajv = new Ajv({verbose: true});
+	configure_ajv(ajv);
+	const discovery = new DataDiscovery(docs, ajv);
+
 	void it('resolves unmatched to raw', async () => {
 		assert.equal(
 			await (await Generator.find([], 'foo')).result(),
+			'foo',
+		);
+		assert.equal(
+			await (await Generator.find([
+				new class extends Generator {
+					constructor() {
+						super(discovery);
+					}
+
+
+					matches() {
+						return Promise.resolve(undefined);
+					}
+				},
+			], 'foo')).result(),
 			'foo'
+		);
+	});
+
+	void it('can resolve matched to something else', async () => {
+		assert.equal(
+			await (await Generator.find([
+				new class extends Generator {
+					constructor() {
+						super(discovery);
+					}
+
+
+					matches() {
+						return Promise.resolve(new RawGenerationResult(
+							'bar'
+						));
+					}
+				},
+			], 'foo')).result(),
+			'bar'
 		);
 	});
 });
