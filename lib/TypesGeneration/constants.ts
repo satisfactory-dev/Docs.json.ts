929384f59ba44c4b5f89968d5dbf2013638f3da8
SignpostMarv <me@signpostmarv.name>
2024-03-31 00:15:29 +0000

    removing unreferenced file

--- lib/TypesGeneration/constants.ts
+++
@@ -1,9 +0,0 @@
-export const schema = {
-	type: 'object',
-	required: ['type', 'const'],
-	additionalProperties: false,
-	properties: {
-		type: {type: 'string', const: 'string'},
-		const: {type: 'string'},
-	},
-};
