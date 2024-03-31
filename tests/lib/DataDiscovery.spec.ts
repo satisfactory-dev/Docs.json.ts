caf94db8c86ef7fc758282d95798b16dbb7af4ed
SignpostMarv <me@signpostmarv.name>
2024-03-31 23:36:34 +0100

    adding test for DataDiscovery.generate_markdown()

--- tests/lib/DataDiscovery.spec.ts
+++ tests/lib/DataDiscovery.spec.ts
@@ -15,6 +15,22 @@ import {
 	array_has_size,
 	value_matches_ExpressionResult,
 } from '../../assert/CustomAssert';
+import {
+	docs,
+	skip_because_docs_dot_json_not_yet_bundled,
+} from '../fixtures/Docs';
+import {
+	__dirname_from_meta,
+} from '../../lib/__dirname';
+import {
+	readFile,
+} from 'node:fs/promises';
+import {
+	configure_ajv,
+} from '../../lib/DocsValidation';
+import Ajv from 'ajv/dist/2020';
+
+const __dirname = __dirname_from_meta(import.meta);
 
 void describe('DataDiscovery.object_literal', () => {
 	void it('handles empty objects', async () => {
@@ -136,3 +152,18 @@ void describe('DataDiscovery.value_literal', () => {
 		);
 	});
 });
+
+void describe(
+	'generate_markdown',
+	skip_because_docs_dot_json_not_yet_bundled,
+	async () => {
+		const ajv = new Ajv({verbose: true});
+		configure_ajv(ajv);
+		const discovery = new DataDiscovery(docs, ajv);
+		const actual = await (discovery.generate_markdown());
+		const expecting = (
+			await readFile(`${__dirname}/../fixtures/data-progress--empty.md`)
+		).toString();
+		assert.equal(actual, expecting);
+	}
+);
