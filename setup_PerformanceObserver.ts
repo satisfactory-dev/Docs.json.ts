f8d80768bc35c4f365df424eac59d01c39d3d533
SignpostMarv <me@signpostmarv.name>
2024-04-08 11:08:27 +0100

    track the number of times a call gets made

--- setup_PerformanceObserver.ts
+++ setup_PerformanceObserver.ts
@@ -1,18 +1,25 @@
 export function setup_PerformanceObserver(clear_lines = true)
 {
-	const measure_totals: {[key: string]: number} = {};
+	const measure_totals: {[key: string]: [number, number]} = {};
 	let last_total_lines = 0;
 
 	const obs = new PerformanceObserver((list) => {
 		for (const entry of list.getEntries()) {
 			if (!(entry.name in measure_totals)) {
-				measure_totals[entry.name] = 0;
+				measure_totals[entry.name] = [0, 0];
 			}
-			measure_totals[entry.name] += entry.duration;
+			measure_totals[entry.name][0] += entry.duration;
+			measure_totals[entry.name][1] += 1;
 		}
 
 		const results = Object.entries(measure_totals).map((e) => {
-			return [e[0], `${(e[1] / 1000).toFixed(4)}s`];
+			return [
+				e[0],
+				{
+					'total time': `${(e[1][0] / 1000).toFixed(4)}s`,
+					amount: e[1][1],
+				},
+			];
 		});
 
 		if (clear_lines && last_total_lines) {
