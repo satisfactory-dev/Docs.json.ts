05c2f515a472e6d2bbfffaf298e6f7b28c83ca0e
SignpostMarv <me@signpostmarv.name>
2024-04-12 12:51:53 +0000

    100% coverage on BasicStringConverter

--- tests/lib/DataDiscovery/JsonSchema/StringType.spec.ts
+++ tests/lib/DataDiscovery/JsonSchema/StringType.spec.ts
@@ -4,6 +4,7 @@ import {
 } from 'node:test';
 import assert from 'node:assert/strict';
 import {
+	BasicStringConverter,
 	ConstStringConverter,
 } from '../../../../lib/DataDiscovery/JsonSchema/StringType';
 import {
@@ -13,6 +14,73 @@ import ts_assert from '@signpostmarv/ts-assert';
 import {
 	rejects_partial_match,
 } from '../../../../assert/CustomAssert';
+import { SchemaObject } from 'ajv';
+
+void describe('BasicStringConverter', () => {
+	const instance = new BasicStringConverter(docs.ajv);
+
+	void describe('can_convert_schema', () => {
+		void it('returns true', () => {
+			assert.equal(
+				instance.can_convert_schema({type: 'string'}),
+				true,
+			);
+		})
+		void it('return false', () => {
+			assert.equal(
+				instance.can_convert_schema({type: 'string', const: 'foo'}),
+				false
+			);
+			assert.equal(
+				instance.can_convert_schema({type: 'number'}),
+				false
+			);
+		})
+	})
+
+	void describe('can_convert_schema_and_raw_data', () => {
+		void it ('resolves true', async () => {
+			const promise = instance.can_convert_schema_and_raw_data(
+				{type: 'string'},
+				'',
+			);
+
+			await assert.doesNotReject(promise);
+			assert.equal(await promise, true);
+		});
+		void it ('resolves false', async () => {
+			const fails:[SchemaObject, unknown][] = [
+				[{type: 'string'}, null],
+				[{type: 'number'}, ''],
+			];
+
+			for (const entry of fails) {
+				const [schema, raw_data] = entry;
+
+				const promise = instance.can_convert_schema_and_raw_data(
+					schema,
+					raw_data,
+				);
+
+				await assert.doesNotReject(promise);
+				assert.equal(await promise, false);
+			}
+		});
+	})
+
+	void describe('convert', () => {
+		void it('behaves', async () => {
+			const promise = instance.convert({type: 'string'}, '');
+
+			await assert.doesNotReject(promise);
+
+			const {expression} = await promise;
+
+			ts_assert.isStringLiteral(expression);
+			assert.equal(expression.text, '');
+		})
+	})
+})
 
 void describe('ConstStringConverter', () => {
 	const instance = new ConstStringConverter(docs.ajv);
