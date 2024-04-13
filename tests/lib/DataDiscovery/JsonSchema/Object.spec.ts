d39c260f539d273c3681d6277597ae3208c24478
SignpostMarv <me@signpostmarv.name>
2024-04-13 03:06:23 +0000

    adjusting indentation

--- tests/lib/DataDiscovery/JsonSchema/Object.spec.ts
+++ tests/lib/DataDiscovery/JsonSchema/Object.spec.ts
@@ -20,53 +20,53 @@ void describe('ObjectConverter', () => {
 	const instance = new ObjectConverter(new DataDiscovery(docs));
 
 	void describe('can_convert_schema_and_raw_data', () => {
-	const schema = {
-		type: 'object',
-		$ref: local_ref('class--no-description-or-display-name'),
-		unevaluatedProperties: false,
-		properties: {
-			foo: {type: 'string'},
-		},
-	};
-
-	assert.equal(
-		instance.can_convert_schema(schema),
-		true,
-		'schema not valid'
-	);
-
-	void it ('resolves false', async () => {
-		assert.equal(
-			await instance.can_convert_schema_and_raw_data(
-				{type: 'string'},
-				null
-			),
-			false,
-			'Should not be able to convert string schema with null'
-		);
+		const schema = {
+			type: 'object',
+			$ref: local_ref('class--no-description-or-display-name'),
+			unevaluatedProperties: false,
+			properties: {
+				foo: {type: 'string'},
+			},
+		};
 
 		assert.equal(
-			await instance.can_convert_schema_and_raw_data(
-				schema,
-				null
-			),
-			false,
-			'Should not be able to convert schema with null value'
-		);
-	});
-
-	void it('resolves true', async () => {
-		assert.equal(
-			await instance.can_convert_schema_and_raw_data(
-				schema,
-				{
-					ClassName: 'foo',
-					foo: 'bar',
-				},
-			),
+			instance.can_convert_schema(schema),
 			true,
-			'Should be able to convert valid schema'
+			'schema not valid'
 		);
-	})
+
+		void it ('resolves false', async () => {
+			assert.equal(
+				await instance.can_convert_schema_and_raw_data(
+					{type: 'string'},
+					null
+				),
+				false,
+				'Should not be able to convert string schema with null'
+			);
+
+			assert.equal(
+				await instance.can_convert_schema_and_raw_data(
+					schema,
+					null
+				),
+				false,
+				'Should not be able to convert schema with null value'
+			);
+		});
+
+		void it('resolves true', async () => {
+			assert.equal(
+				await instance.can_convert_schema_and_raw_data(
+					schema,
+					{
+						ClassName: 'foo',
+						foo: 'bar',
+					},
+				),
+				true,
+				'Should be able to convert valid schema'
+			);
+		})
 	})
 });
