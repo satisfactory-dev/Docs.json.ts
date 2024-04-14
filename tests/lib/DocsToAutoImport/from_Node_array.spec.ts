48ba8e3341b8186e2b2cd39ac9735593fb24946c
SignpostMarv <me@signpostmarv.name>
2024-04-14 06:43:52 +0000

    increasing coverage a little

---
+++ tests/lib/DocsToAutoImport/from_Node_array.spec.ts
@@ -0,0 +1,69 @@
+import {
+	describe,
+	it,
+} from 'node:test';
+import assert from 'node:assert/strict';
+import {
+	EntityName_array_from_Node_array,
+} from '../../../lib/DocsToAutoImport/from_Node_array';
+import ts, {
+	Node,
+} from 'typescript';
+
+void describe('EntityName_array_from_Node_array', () => {
+	const empty_behaves:{[key: string]: Node[]} = {
+		'[]': [],
+		ClassDeclaration: [
+			ts.factory.createClassDeclaration(
+				undefined,
+				undefined,
+				undefined,
+				undefined,
+				[]
+			),
+		],
+		FunctionDeclaration: [
+			ts.factory.createFunctionDeclaration(
+				undefined,
+				undefined,
+				undefined,
+				undefined,
+				[],
+				undefined,
+				undefined
+			),
+		],
+		MethodDeclaration: [
+			ts.factory.createMethodDeclaration(
+				undefined,
+				undefined,
+				'foo',
+				undefined,
+				undefined,
+				[],
+				undefined,
+				undefined,
+			),
+		],
+		PropertyDeclaration: [
+			ts.factory.createPropertyDeclaration(
+				undefined,
+				'foo',
+				undefined,
+				undefined,
+				undefined,
+			),
+		],
+		TupleTypeNode: [
+			ts.factory.createTupleTypeNode([]),
+		],
+	};
+
+	for (const entry of Object.entries(empty_behaves)) {
+		void it (`empty ${entry[0]} behaves`, () => {
+			const result = EntityName_array_from_Node_array(entry[1]);
+
+			assert.deepEqual(result, []);
+		})
+	}
+})
