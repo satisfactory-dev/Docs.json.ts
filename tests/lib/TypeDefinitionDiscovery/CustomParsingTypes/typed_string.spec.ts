4b0f87c110add8a2f31f8b474ff5cb4f3b16eb0b
SignpostMarv <me@signpostmarv.name>
2024-04-14 07:57:23 +0000

    full coverage of array type branch

--- tests/lib/TypeDefinitionDiscovery/CustomParsingTypes/typed_string.spec.ts
+++ tests/lib/TypeDefinitionDiscovery/CustomParsingTypes/typed_string.spec.ts
@@ -7,23 +7,24 @@ import assert from 'node:assert/strict';
 import {
 	typed_string,
 } from '../../../../lib/TypeDefinitionDiscovery/CustomParsingTypes/typed_string';
-/*
 import {
 	TypeDefinitionWriter,
 } from '../../../../lib/TypeDefinitionWriter';
 import {
 	docs,
 } from '../../../../lib/helpers';
-*/
 import {
 	typed_string_inner_array_prefixItems_type,
 	typed_string_inner_array_type,
 	typed_string_inner_object_pattern_type,
 	typed_string_inner_object_type,
 } from '../../../../lib/CustomParsingTypes/TypedString';
+import ts_assert from '@signpostmarv/ts-assert';
+import {
+	array_has_size,
+} from '../../../../assert/CustomAssert';
 
-void describe('typed_string', () => {
-	/*
+void describe('typed_string', async () => {
 	const type_definition_writer = new TypeDefinitionWriter(docs);
 	const discovery = await type_definition_writer.discovery;
 	const discovered_types = (
@@ -34,7 +35,6 @@ void describe('typed_string', () => {
 		discovered_types,
 		discovery
 	);
-	*/
 
 	const static_schema_checks:[
 		(
@@ -139,6 +139,53 @@ void describe('typed_string', () => {
 					expectation
 				);
 			}
+		})
+	})
+
+	void describe('generate', () => {
+		void it ('behaves with found array type', () => {
+			const result = instance.generate()({
+				type: 'string',
+				minLength: 1,
+				typed_string: {
+					minItems: 1,
+					items: {
+						type: 'string',
+						const: 'foo',
+					},
+				},
+			});
+
+			ts_assert.isTupleTypeNode(result);
+			array_has_size(result.elements, 2);
+			ts_assert.isLiteralTypeNode(result.elements[0]);
+			ts_assert.isStringLiteral(result.elements[0].literal);
+			ts_assert.isRestTypeNode(result.elements[1]);
+			ts_assert.isArrayTypeNode(result.elements[1].type);
+			ts_assert.isLiteralTypeNode(
+				result.elements[1].type.elementType
+			);
+			ts_assert.isStringLiteral(
+				result.elements[1].type.elementType.literal
+			);
+		})
+		void it('behaves with known array type', () => {
+			const result = instance.generate()({
+				type: 'string',
+				minLength: 1,
+				typed_string: {
+					minItems: 1,
+					items: {
+						type: 'string',
+						minLength: 1,
+						UnrealEngineString: true,
+					},
+				},
+			});
+
+			ts_assert.isTupleTypeNode(result);
+			array_has_size(result.elements, 2);
+			ts_assert.isTypeReferenceNode(result.elements[0]);
 		})
 	})
 })
