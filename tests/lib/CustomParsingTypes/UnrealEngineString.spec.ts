cb530f4fd29cce65fdb730ac225bfed29efbf394
SignpostMarv <me@signpostmarv.name>
2024-04-23 09:31:53 +0000

    increase coverage

--- tests/lib/CustomParsingTypes/UnrealEngineString.spec.ts
+++ tests/lib/CustomParsingTypes/UnrealEngineString.spec.ts
@@ -5,6 +5,8 @@ import {
 import assert from 'node:assert/strict';
 import {
 	string_or_string_array_to_node,
+	UnrealEngineString,
+	UnrealEngineString_type,
 } from '../../../lib/CustomParsingTypes/UnrealEngineString';
 import ts_assert from '@signpostmarv/ts-assert';
 import {
@@ -25,3 +27,162 @@ void describe('string_or_string_array_to_node', () => {
 		assert.equal(value.types[1].literal.text, 'bar');
 	});
 });
+
+const regex_uppercase = 'A-Z';
+const regex_alpha = `${regex_uppercase}a-z`;
+
+const general_regex =
+	`/(?:[${
+		regex_uppercase
+	}-][A-Za-z0-9_-]+/)+(?:[${
+		regex_uppercase
+	}][A-Za-z_0-9-]+\\.[${
+		regex_uppercase
+	}][A-Za-z_0-9-]+(?:_C)?(?::[${
+		regex_uppercase
+	}][A-Za-z0-9_]+)?|[${
+		regex_uppercase
+	}][${
+		regex_alpha
+	}_]+\\.[${
+		regex_uppercase
+	}][${
+		regex_alpha
+	}_]+)`;
+
+const right_regex_suffix =
+	`(?:[${
+		regex_uppercase
+	}][${
+		regex_alpha
+	}0-9_.]+/)*[${
+		regex_uppercase
+	}][${
+		regex_alpha
+	}_.0-9-]+(?::[${
+		regex_uppercase
+	}][${
+		regex_alpha
+	}0-9]+)?`;
+
+void describe('UnrealEngineString', () => {
+	void describe('ajv_macro_generator', () => {
+		void describe('result', () => {
+			const data_sets:[
+				UnrealEngineString_type,
+				string,
+			][] = [
+				[
+					true,
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:${
+						general_regex
+					}|"${
+						general_regex
+					}")')`,
+				],
+				[
+					{left: 'foo'},
+					`(?:(?:foo)'(?:${
+						general_regex
+					}|"${
+						general_regex
+					}")')`,
+				],
+				[
+					{left: ['foo']},
+					`(?:(?:foo)'(?:${
+						general_regex
+					}|"${
+						general_regex
+					}")')`,
+				],
+				[
+					{left: ['foo', 'bar']},
+					`(?:(?:foo|bar)'(?:${
+						general_regex
+					}|"${
+						general_regex
+					}")')`,
+				],
+				[
+					{right: 'foo'},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:foo)|"(?:foo)")')`,
+				],
+				[
+					{right: ['foo']},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:foo)|"(?:foo)")')`,
+				],
+				[
+					{right: ['foo', 'bar']},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:foo|bar)|"(?:foo|bar)")')`,
+				],
+				[
+					{right: {starts_with: 'foo'}},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:(?:foo${
+						right_regex_suffix
+					}))|"(?:(?:foo${
+						right_regex_suffix
+					}))")')`,
+				],
+				[
+					{right: {starts_with: ['foo']}},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:(?:foo${
+						right_regex_suffix
+					}))|"(?:(?:foo${
+						right_regex_suffix
+					}))")')`,
+				],
+				[
+					{right: {starts_with: ['foo', 'bar']}},
+					`(?:(?:/Script/Engine.BlueprintGeneratedClass)'(?:(?:(?:foo${
+						right_regex_suffix
+					}|bar${
+						right_regex_suffix
+					}))|"(?:(?:foo${
+						right_regex_suffix
+					}|bar${
+						right_regex_suffix
+					}))")')`,
+				],
+			];
+			const inner = UnrealEngineString.ajv_macro_generator(
+				true
+			);
+			const standalone = UnrealEngineString.ajv_macro_generator(
+				false
+			);
+
+			for (const data_set of data_sets) {
+				const [data, pattern] = data_set;
+				void it(
+					`UnrealEngineString.ajv_macro_generator(true)(${
+						JSON.stringify(data)
+					}) returns {pattern: ${pattern}}`,
+					() => {
+
+						assert.deepEqual(
+							inner(data),
+							{
+								pattern,
+							}
+						);
+					}
+				)
+				void it(
+					`UnrealEngineString.ajv_macro_generator(false)(${
+						JSON.stringify(data)
+					}) returns {pattern: ${pattern}}`,
+					() => {
+
+						assert.deepEqual(
+							standalone(data),
+							{
+								pattern: `^${pattern}$`,
+							}
+						);
+					}
+				)
+			}
+		})
+	})
+})
