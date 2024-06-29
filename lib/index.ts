0cd8c1556cbf0f2cd0e5d1f2e10ac2f5d2447e98
SignpostMarv <me@signpostmarv.name>
2024-06-29 03:28:40 +0000

    additional export

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from './AjvUtilities';
 export * from './ArrayUtilities';
+export * from './CustomParsingTypes/CustomPairingTypes';
 export * from './DocsTsGenerator';
 export * from './DocsValidation';
 export * from './Exceptions';
