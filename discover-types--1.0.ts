e3f1cf05405199fc96d21b1eb8b22667aad89252
SignpostMarv <me@signpostmarv.name>
2024-09-23 12:22:31 +0000

    adjusting indentation

--- discover-types--1.0.ts
+++ discover-types--1.0.ts
@@ -26,56 +26,56 @@ const perf = setup_PerformanceObserver();
 
 const version = 'version_1_0_0_0';
 const sub_path = versions.version_1_0_0_0;
-	try {
-		performance.mark('start');
-		const bar = new TypeDefinitionWriter(
-			docs,
-			version,
-		);
-		performance.measure('bootstrap', 'start');
-		performance.mark('bootstrap done');
-		await bar.write(`${__dirname}/generated-types/${sub_path}/`);
-		performance.measure('types generated', 'bootstrap done');
-		const discovery = await bar.discovery;
-		const result = await discovery.discover_type_$defs();
+try {
+	performance.mark('start');
+	const bar = new TypeDefinitionWriter(
+		docs,
+		version,
+	);
+	performance.measure('bootstrap', 'start');
+	performance.mark('bootstrap done');
+	await bar.write(`${__dirname}/generated-types/${sub_path}/`);
+	performance.measure('types generated', 'bootstrap done');
+	const discovery = await bar.discovery;
+	const result = await discovery.discover_type_$defs();
 
-		process.stdout.write(
-			`${
-				JSON.stringify(result.missing_classes, null, '\t')
-			}\n`,
-		);
-		console.table({
-			'Found Types': Object.keys(result.found_types).length,
-			'Missing Types': result.missing_types.length,
-			'Found Classes': result.found_classes.length,
-			'Missing Classes': result.missing_classes.length,
-		});
+	process.stdout.write(
+		`${
+			JSON.stringify(result.missing_classes, null, '\t')
+		}\n`,
+	);
+	console.table({
+		'Found Types': Object.keys(result.found_types).length,
+		'Missing Types': result.missing_types.length,
+		'Found Classes': result.found_classes.length,
+		'Missing Classes': result.missing_classes.length,
+	});
+	await writeFile(
+		`${__dirname}/discover-types.${sub_path}.perf.json`,
+		`${JSON.stringify(perf(), null, '\t')}`,
+	);
+} catch (err) {
+	await writeFile(
+		`${__dirname}/discover-types.${sub_path}.perf.json`,
+		`${JSON.stringify(perf(), null, '\t')}`,
+	);
+	if (err instanceof NoMatchError) {
+		console.error('ran into an issue');
 		await writeFile(
-			`${__dirname}/discover-types.${sub_path}.perf.json`,
-			`${JSON.stringify(perf(), null, '\t')}`,
+			`./discovery-types.${sub_path}.failure.json`,
+			JSON.stringify(
+				{
+					property: err.property as unknown,
+					message: err.message,
+					stack: err.stack?.split('\n'),
+				},
+				null,
+				'\t',
+			),
 		);
-	} catch (err) {
-		await writeFile(
-			`${__dirname}/discover-types.${sub_path}.perf.json`,
-			`${JSON.stringify(perf(), null, '\t')}`,
-		);
-		if (err instanceof NoMatchError) {
-			console.error('ran into an issue');
-			await writeFile(
-				`./discovery-types.${sub_path}.failure.json`,
-				JSON.stringify(
-					{
-						property: err.property as unknown,
-						message: err.message,
-						stack: err.stack?.split('\n'),
-					},
-					null,
-					'\t',
-				),
-			);
 
-			console.error(err.message, err.stack);
-		} else {
-			throw err;
-		}
+		console.error(err.message, err.stack);
+	} else {
+		throw err;
 	}
+}
