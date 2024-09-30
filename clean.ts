60adff8029e37b3af81d4bfeb07f3e2648a60e62
SignpostMarv <me@signpostmarv.name>
2024-09-30 21:31:07 +0000

    updating clean to clear out generated types

--- clean.ts
+++ clean.ts
@@ -10,3 +10,7 @@ const __dirname = import.meta.dirname;
 for (const remove of await glob(`${__dirname}/{data/*.mjs,coverage/tmp/*.json}`)) {
 	await unlink(remove);
 }
+
+for (const remove of await glob(`${__dirname}/generated-types/{update8,1.0}/**/*.ts`)) {
+	await unlink(remove);
+}
