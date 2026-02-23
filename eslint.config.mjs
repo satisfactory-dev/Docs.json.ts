02bc0e3c4e932f4f781b0f25be267aaf4f1e26bc
SignpostMarv <me@signpostmarv.name>
2026-02-23 12:13:58 +0000

    updating devcontainer / eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import config from '@signpostmarv/eslint-config';
+import {
+	typescript as config,
+} from '@signpostmarv/eslint-config';
 import parser from '@typescript-eslint/parser';
-import imports from 'eslint-plugin-import';
 
 export default [
 	{
@@ -15,22 +16,5 @@ export default [
 	{
 		files: ['**/*.ts', './src/**/*.ts'],
 		ignores: ['**/*.d.ts', '**/*.js', '**/*.mjs'],
-	},
-	{
-		plugins: {
-			imports,
-		},
-		rules: {
-			'imports/no-internal-modules': ['error', {
-				allow: [
-					'ajv/dist/2020.js',
-					`${import.meta.dirname}/data/**/*.json`,
-					`${import.meta.dirname}/schema/**/*.json`,
-					`${import.meta.dirname}/src/*.ts`,
-					`${import.meta.dirname}/src/**/*.ts`,
-					`${import.meta.dirname}/generated-types/**/*.ts`,
-				],
-			}],
-		},
 	},
 ];
