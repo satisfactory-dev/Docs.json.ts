ebe2ca28f65a31ff8861cf9729a5a7e15c4591dc
SignpostMarv <me@signpostmarv.name>
2026-03-10 21:49:04 +0000

    adding utility type
    we don't yet have a custom schema for defining the NativeClass type,
    so we're adding this utility type here for use in downstream projects.

--- src/types.ts
+++ src/types.ts
@@ -24,3 +24,14 @@ export type semver_full<
 		PositiveIntegerOrZero<number>
 	),
 > = `${Major}.${Minor}.${Patch}.${Build}`;
+
+export type NativeClass<
+	T0 extends string,
+	T1 extends [
+		{[key: string]: unknown} & {ClassName: `${string}_C`},
+		...{[key: string]: unknown} & {ClassName: `${string}_C`}[],
+	],
+> = {
+	NativeClass: `/Script/CoreUObject.Class'/Script/FactoryGame.${T0}'`,
+	Classes: T1,
+};
