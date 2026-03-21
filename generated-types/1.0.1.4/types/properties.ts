21f391028ddcc626bc22a0d43daa293dd20031de
SignpostMarv <me@signpostmarv.name>
2026-03-21 01:58:16 +0000

    consistent sort order

--- generated-types/1.0.1.4/types/properties.ts
+++ generated-types/1.0.1.4/types/properties.ts
@@ -35,14 +35,14 @@ import type {
 } from './../../0.4.2.11/types/properties.ts';
 
 import type {
-	mUnlocks_item_base as docs_json_ts_0_3_7_7_properties_mUnlocks_item_base,
-} from './../../0.3.7.7/types/properties.ts';
-
-import type {
 	CameraAnimationSequence_quoted as docs_json_ts_0_8_3_3_properties_overridable_CameraAnimationSequence_quoted,
 	DocsDotJson_AkAudioEvent_quoted_list_item as docs_json_ts_0_8_3_3_properties_overridable_DocsDotJson_AkAudioEvent_quoted_list_item,
 	mUnlocks_item as docs_json_ts_0_8_3_3_properties_overridable_mUnlocks_item,
 } from './../../0.8.3.3/types/properties.1.0.1.4.ts';
+
+import type {
+	mUnlocks_item_base as docs_json_ts_0_3_7_7_properties_mUnlocks_item_base,
+} from './../../0.3.7.7/types/properties.ts';
 
 type mClearanceData_item = {
 	Type?: 'CT_Soft',
@@ -100,13 +100,11 @@ type mStatisticGameplayTag = {
 	TagName: `Stats.Gameplay.Schematics.Schematic${'1-1' | '1-2' | '1-3' | '2-1' | '2-2' | '2-3' | '2-5' | '3-1' | '3-2' | '3-3' | '3-4' | '4-1' | '4-2' | '4-3' | '4-4' | '4-5' | '5-1' | '5-2' | '5-3' | '5-4' | '5-5' | '6-1' | '6-2' | '6-3' | '6-4' | '6-5' | '6-6' | '6-7' | '7-1' | '7-2' | '7-3' | '7-4' | '7-5' | '8-1' | '8-2' | '8-3' | '8-4' | '8-5' | '9-1' | '9-2' | '9-3' | '9-4' | '9-5'}`,
 };
 
+type mUnlocks_item = mUnlocks_item_mCustomizationUnlocks | mUnlocks_item_mUploadSpeedPercentageDecrease | mUnlocks_item_mItemStackLimitIncrease | mUnlocks_item_mNumSlotsToUnlock | docs_json_ts_0_8_3_3_properties_overridable_mUnlocks_item;
+
 type mUnlocks_item_mCustomizationUnlocks = {
 	Class: 'FGUnlockCustomization',
 	mCustomizationUnlocks: docs_json_ts_0_3_7_7_overridable_DocsDotJson_BlueprintGeneratedClass_quoted_list,
-};
-
-type mUnlocks_item_mUploadSpeedPercentageDecrease = docs_json_ts_0_3_7_7_properties_mUnlocks_item_base & {
-	mUploadSpeedPercentageDecrease: docs_json_ts_common_types_decimal_string,
 };
 
 type mUnlocks_item_mItemStackLimitIncrease = docs_json_ts_0_3_7_7_properties_mUnlocks_item_base & {
@@ -117,7 +115,9 @@ type mUnlocks_item_mNumSlotsToUnlock = docs_json_ts_0_3_7_7_properties_mUnlocks_
 	mNumSlotsToUnlock: docs_json_ts_common_types_integer_string,
 };
 
-type mUnlocks_item = mUnlocks_item_mCustomizationUnlocks | mUnlocks_item_mUploadSpeedPercentageDecrease | mUnlocks_item_mItemStackLimitIncrease | mUnlocks_item_mNumSlotsToUnlock | docs_json_ts_0_8_3_3_properties_overridable_mUnlocks_item;
+type mUnlocks_item_mUploadSpeedPercentageDecrease = docs_json_ts_0_3_7_7_properties_mUnlocks_item_base & {
+	mUploadSpeedPercentageDecrease: docs_json_ts_common_types_decimal_string,
+};
 
 type FGItemDescriptor_version_specific_mDescriptorStatBars_item = docs_json_ts_0_6_1_5_properties_FGItemDescriptor_version_specific_mDescriptorStatBars_item | docs_json_ts_common_types_empty_object;
 
@@ -433,11 +433,11 @@ export type {
 	mSchematicDependencies_item,
 	mSchematicDependencies_item_version_specific,
 	mStatisticGameplayTag,
+	mUnlocks_item,
 	mUnlocks_item_mCustomizationUnlocks,
-	mUnlocks_item_mUploadSpeedPercentageDecrease,
 	mUnlocks_item_mItemStackLimitIncrease,
 	mUnlocks_item_mNumSlotsToUnlock,
-	mUnlocks_item,
+	mUnlocks_item_mUploadSpeedPercentageDecrease,
 	FGItemDescriptor_version_specific_mDescriptorStatBars_item,
 	FGJetPackFuelParameters_quoted,
 	FGSchematic_has_mSchematicDependencies,
